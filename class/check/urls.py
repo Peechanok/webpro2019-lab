0bd20912f1e9e2255da436baa658fb7ddff4f224
Peechanok <61070146@kmitl.ac.th>
2020-02-06 01:36:32 +0700

    เพิ่ม home

--- class/check/urls.py
+++ class/check/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('list_sub/<str:username>',views.check_list, name = 'list_sub'),
-    path('course/<int:stu_not>',views.Details, name = 'Details'),
-    path('chackname/<str:subject>',views.Qr_code, name = 'Qr_code'),
+ path('list_sub/<str:username>',views.check_list, name = 'list_sub_page'),
+    path('course/<int:stu_not>',views.Details, name = 'Details_page'),
+    path('chackname/<str:subject>',views.Qr_code, name = 'Qr_code_page'),
+     path('error404/',views.show_error_404, name = 'show_error_404_page'),
+     path('home/',views.home, name = 'home_page'),
 ]
--- class/check/views.py
+++ class/check/views.py
@@ -1,5 +1,8 @@
+
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
 # Create your views here.
 def check_list(request,username):
     return HttpResponse( 'welcome to %s Today is Thursday.\n You have subjects that are taught are ENGLISH , MATH , WEB PROGRAMING ' % username)
@@ -7,3 +10,16 @@ def Details(request,stu_not):
     return HttpResponse( 'This course teaches englist there are 100 students, there are come 20 students and %s lack.' % stu_not)
 def Qr_code(request,subject):
     return HttpResponse( 'Name Subject : %s   Page name check system with qr code ' % subject)
+
+
+
+def home(request):
+    return render(request,template_name='check/home.html')
+
+
+def show_error_404(request):
+    foo = True
+    if foo:
+        return HttpResponseNotFound('<h1> This is not fond </h1>')
+    else:
+        return redirect(to='home_page')
